4b543419e29b7f1920d4fe686dc6398866bff2ad
Charlotte Capitanchik <charlotte.capitanchik@crick.ac.uk>
2020-04-02 12:45:44 +0100

    edit to make slightly more functional but still not working properly
    this still isn't quite right.

--- run_scripts/example-run-camp-data.sh
+++ run_scripts/example-run-camp-data.sh
@@ -4,44 +4,32 @@
 ## Copy this script to a working folder
 
 ## Help stuff and param setting
-usage="$(basename "$0") [-h] [-reads folder] [-bowtie_rRNA_index folder] [-genome_star_index folder] [-genome_fai genome_index_file] [-] \
+usage="$(basename "$0") [-h] [-r folder] [-b folder] [-s folder] [-f genome_index_file] [-] \
 -- Run Nextflow CLIP pipeline on CAMP with your own data.
 
-where:
+where REQUIRED parameters are:
     -h  show this help text
-    -reads  give the directory where your reads live, make sure they have ending .fq.qz not .fastq.gz
-    -bowtie_rRNA_index  give the directory for your pre-mapping bowtie index
-    -genome_star_index directory for genome STAR index
-    -genome_fai path to genome fasta index file"
+    -r  give the directory where your reads live, make sure they have ending .fq.qz not .fastq.gz
+    -b  give the directory for your pre-mapping bowtie index
+    -s directory for genome STAR index
+    -f path to genome fasta index file"
 
-while getopts ':hs:' option; do
+while getopts ':r:b:s:f:h' option; do
   case "$option" in
+    :) printf "\-$OPTARG requires an argument\n" >&2
+       echo "$usage" >&2
+       exit 1
+       ;;
+    r) reads=$OPTARG
+       ;;
+    b) bowtie_index=$OPTARG
+       ;;
+    s) star_index=$OPTARG
+       ;;
+    f) genome_fai=$OPTARG
+       ;;
     h) echo "$usage"
        exit
-       ;;
-    reads) reads=$OPTARG
-       ;;
-    :) printf "missing argument for -%reads\n" "$OPTARG" >&2
-       echo "$usage" >&2
-       exit 1
-       ;;
-    bowtie_rRNA_index) bowtie_index=$OPTARG
-       ;;
-    :) printf "missing argument for -%bowtie_rRNA_index\n" "$OPTARG" >&2
-       echo "$usage" >&2
-       exit 1
-       ;;
-    genome_star_index) star_index=$OPTARG
-       ;;
-    :) printf "missing argument for -%genome_star_index\n" "$OPTARG" >&2
-       echo "$usage" >&2
-       exit 1
-       ;;
-    genome_fai) genome_fai=$OPTARG
-       ;;
-    :) printf "missing argument for -%genome_fai\n" "$OPTARG" >&2
-       echo "$usage" >&2
-       exit 1
        ;;
    \?) printf "illegal option: -%s\n" "$OPTARG" >&2
        echo "$usage" >&2
@@ -49,8 +37,18 @@ while getopts ':hs:' option; do
        ;;
   esac
 done
+if ((OPTIND == 1))
+then
+    echo "$usage" >&2
+    exit 1
+fi
 shift $((OPTIND - 1))
-
+if ! $reads || ! $bowtie_index || ! $star_index || ! $genome_fai
+then
+    echo "You are missing key parameters...\n"
+    echo "$usage" >&2
+    exit 1
+fi
 
 ## LOAD REQUIRED MODULES
 ml purge
