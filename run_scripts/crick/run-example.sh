6a8ae4f09e09f58f9a4e3d23b60ec4f2c2073059
Sviatoslav Sidorov <sviatoslav.sidorov@crick.ac.uk>
2020-05-06 20:05:31 +0100

    Corrected example run script and added example metadata csv

--- run_scripts/crick/run-example.sh
+++ run_scripts/crick/run-example.sh
@@ -14,9 +14,9 @@ nextflow pull luslab/group-nextflow-clip
 
 ## RUN PIPELINE
 nextflow run luslab/group-nextflow-clip \
-  -r dev \
+  -r 'dev' \
   -profile crick \
-  --reads '/camp/lab/luscomben/working/charlotte/SM/hg38_Gencode29_BPs_LaBranchoR/PRP8_rupert/iMaps_cutadapt_fastqs' \
+  --input 'metadata-example.csv' \
   --bowtie_index '/camp/lab/luscomben/working/charlotte/tRNA_rRNA_for_iMaps/Homo_sapiens/small_rna_bowtie' \
   --star_index '/camp/lab/luscomben/working/charlotte/miCLIP_UN1710_CS2_delta2/summarising_trna_mapping_style/tRNA_paper/results/genome_star_index' \
   --genome_fai '/camp/lab/luscomben/working/charlotte/spliceosome_iCLIP/permanent_files/GRCh38.primary_assembly.genome.fa.fai'
